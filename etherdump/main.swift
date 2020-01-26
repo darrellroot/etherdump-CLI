2d263a5d27968da1c5176adee411ff6abbd5450f
Darrell Root <darrellrootjunk@mac.com>
2020-01-26 14:59:04 -0800

    Initial Commit

---
+++ etherdump/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  etherdump
+//
+//  Created by Darrell Root on 1/26/20.
+//  Copyright © 2020 com.darrellroot. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
