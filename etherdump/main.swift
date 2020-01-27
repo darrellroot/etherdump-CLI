5a7c180686fceec4f1dc5b6a29ad29a715042196
Darrell Root <darrellrootjunk@mac.com>
2020-01-26 16:04:56 -0800

    First runnable version

--- etherdump/main.swift
+++ etherdump/main.swift
@@ -1,12 +1,28 @@
 //
 //  main.swift
-//  etherdump
+//  packetCapture1
 //
-//  Created by Darrell Root on 1/26/20.
+//  Created by Darrell Root on 1/23/20.
 //  Copyright © 2020 com.darrellroot. All rights reserved.
 //
 
 import Foundation
+import PackageEtherCapture
 
 print("Hello, World!")
 
+guard let etherCapture = EtherCapture(interface: "en0", command: "") else {
+    fatalError("failed to initialize SwiftPcap")
+}
+
+DispatchQueue.main.asyncAfter(deadline: .now() + 5.0) {
+    var done = false
+    repeat {
+        if let frame = etherCapture.nextPacket() {
+            debugPrint(frame.description)
+        } else {
+            done = true
+        }
+    } while !done
+}
+RunLoop.current.run()
